36d029d1cb727760cda1099d820bd23efa4ff649
Adophilus <uchenna19of@gmail.com>
2026-04-30 18:51:36 +0100

    feat: finished working on author module

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AuthorService } from './author.service';
-import { CreateAuthorDto } from './dto/create-author.dto';
-import { UpdateAuthorDto } from './dto/update-author.dto';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Post,
+	Put,
+} from "@nestjs/common";
+import { AuthorService } from "./author.service";
+import { CreateAuthorDto } from "./dto/create-author.dto";
+import { UpdateAuthorDto } from "./dto/update-author.dto";
+import { BookService } from "../book/book.service";
+import { VideoService } from "../video/video.service";
+import { BookViewService } from "../book-view/book-view.service";
+import { VideoViewService } from "../video-view/video-view.service";
 
-@Controller('author')
+@Controller("authors")
 export class AuthorController {
-  constructor(private readonly authorService: AuthorService) {}
+	constructor(
+		private readonly authorService: AuthorService,
+		private readonly bookService: BookService,
+		private readonly videoService: VideoService,
+		private readonly bookViewService: BookViewService,
+		private readonly videoViewService: VideoViewService,
+	) {}
 
-  @Post()
-  create(@Body() createAuthorDto: CreateAuthorDto) {
-    return this.authorService.create(createAuthorDto);
-  }
+	@Post()
+	create(@Body() createAuthorDto: CreateAuthorDto) {
+		return this.authorService.create(createAuthorDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.authorService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.authorService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authorService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id") id: string) {
+		return this.authorService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
-    return this.authorService.update(+id, updateAuthorDto);
-  }
+	@Put(":id")
+	update(@Param("id") id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
+		return this.authorService.update(+id, updateAuthorDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authorService.remove(+id);
-  }
+	@Delete(":id")
+	remove(@Param("id") id: string) {
+		return this.authorService.remove(+id);
+	}
+
+	@Get(":authorId/books")
+	findBooks(@Param("authorId") authorId: string) {
+		return this.bookService.findByAuthor(+authorId);
+	}
+
+	@Get(":authorId/videos")
+	findVideos(@Param("authorId") authorId: string) {
+		return this.videoService.findByAuthor(+authorId);
+	}
+
+	@Get(":authorId/book-views")
+	findBookViews(@Param("authorId") authorId: string) {
+		return this.bookViewService.findByAuthor(+authorId);
+	}
+
+	@Get(":authorId/video-views")
+	findVideoViews(@Param("authorId") authorId: string) {
+		return this.videoViewService.findByAuthor(+authorId);
+	}
 }
--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AuthorService } from './author.service';
-import { AuthorController } from './author.controller';
+import { Module } from "@nestjs/common";
+import { AuthorController } from "./author.controller";
+import { AuthorService } from "./author.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Author } from "./entities/author.entity";
+import { BookModule } from "../book/book.module";
+import { VideoModule } from "../video/video.module";
+import { BookViewModule } from "../book-view/book-view.module";
+import { VideoViewModule } from "../video-view/video-view.module";
 
 @Module({
-  controllers: [AuthorController],
-  providers: [AuthorService],
+	imports: [
+		TypeOrmModule.forFeature([Author]),
+		BookModule,
+		VideoModule,
+		BookViewModule,
+		VideoViewModule,
+	],
+	controllers: [AuthorController],
+	providers: [AuthorService],
+	exports: [AuthorService],
 })
 export class AuthorModule {}
