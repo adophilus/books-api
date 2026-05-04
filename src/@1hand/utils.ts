949bb1f2f192603a1508f761e3d8e50c63529110
Adophilus <uchenna19of@gmail.com>
2026-05-04 12:04:09 +0100

    chore: reverted the changes made to utils

--- src/@1hand/utils.ts
+++ src/@1hand/utils.ts
@@ -1,3 +1,4 @@
+import { Account } from '@prisma/client';
 import * as bcrypt from 'bcryptjs';
 import { randomBytes } from 'crypto';
 import slugify from 'slugify';
@@ -64,13 +65,6 @@ export const generateMatricule = (role: string): string => {
       PAYMENT_CONFIG: 'PC',
       STUDENT_PAYMENT: 'SP',
       PAYMENT_DISCOUNT: 'PD',
-      AUTHOR: 'AUT',
-      BOOK: 'BOK',
-      VIDEO: 'VID',
-      BOOK_VIEW: 'BKV',
-      VIDEO_VIEW: 'VDV',
-      BOOK_COMMENT: 'BKC',
-      VIDEO_COMMENT: 'VDC',
     }[role] || 'E';
 
   // Générer 4 caractères alphanumériques
